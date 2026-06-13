1fddfb3fe150c646b61300389f5521942ac2b84a
Mohammed <mohammed.secure@outlook.sa>
2026-06-13 16:57:40 +0300

    Task 3: Printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -30,3 +30,11 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
